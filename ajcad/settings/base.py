713b1eb48012bdb090dbd2e5cf05dac9394f146e
Williams de SOUZA <williamsko89@gmail.com>
2021-05-05 13:41:54 +0200

    enable database sqlite

--- ajcad/settings/base.py
+++ ajcad/settings/base.py
@@ -171,7 +171,7 @@ DATABASES = {
 
     'default': {
         'ENGINE': os.getenv('DATABASE_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': os.getenv('DATABASE_NAME', BASE_DIR / 'db.sqlite3'),
+        'NAME': os.getenv('DATABASE_NAME', 'db.sqlite3'),
         'USER': os.getenv('DATABASE_USERNAME', 'myprojectuser'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
         'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
